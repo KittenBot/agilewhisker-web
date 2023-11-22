60335ec8423e783999c34033675bf0c744417871
riven <4771215@qq.com>
2023-11-22 13:59:16 +0800

    update keyboard ui

--- src/pages/keymap.tsx
+++ src/pages/keymap.tsx
@@ -3,16 +3,30 @@ import Layout from '@theme/Layout';
 import Keyboard from "react-simple-keyboard"
 import "react-simple-keyboard/build/css/index.css"
 
+import "./keymap.css"
+
 const layout = {
     'default': [
-        '{esc} 1 2 3 4 5 6 7 8 9 0 - = {bksp} ⭕',
+        '{esc} 1 2 3 4 5 6 7 8 9 0 - = {backspace} {encoder}',
         '{tab} q w e r t y u i o p [ ] \\',
-        '{lock} a s d f g h j k l ; \' {enter}',
-        '{shift} z x c v b n m , . / {shift} FN ▲ DEL',
-        'CTL WIN ALT {space} FN CTL FN ◀ ▼ ▶'
-    ]
+        "{capslock} a s d f g h j k l ; ' {enter}",
+        '{shiftleft} z x c v b n m , . / {shiftright} FN {arrowup} FN2',
+        '{controlleft} {metaleft} {altleft} {space} {altright} {metaright} {ctrlright} {arrowleft} {arrowdown} {arrowright}'
+    ],
 }
 
+const display = {
+    "{backspace}": "⌫",
+    "{encoder}": "⭕",
+    "{enter}": "⏎",
+    "{controlleft}": "Ctrl ^",
+    "{metaleft}": "Win ⌘",
+    "{altleft}": "Alt ⌥",
+    "{altright}": "Alt ⌥",
+    "{metaright}": "Win ⌘",
+    "{ctrlright}": "Ctrl ^",
+
+}
 
 export default function Keymap() {
   return (
@@ -20,8 +34,12 @@ export default function Keymap() {
     <div>
       <h1>Keymap</h1>
       <Keyboard 
+        debug={true}
         layout={layout}
-      
+        display={display}
+        mergeDisplay={true}
+        syncInstanceInputs={true}
+        physicalKeyboardHighlight={true}
       />
     </div>
     </Layout>
