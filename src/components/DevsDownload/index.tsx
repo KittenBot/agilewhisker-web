2e3af2bd692a34b8294d4ec64384e2fbb6ebe4ed
riven <4771215@qq.com>
2023-11-29 10:04:46 +0800

    devs compile on client only

--- src/components/DevsDownload/index.tsx
+++ src/components/DevsDownload/index.tsx
@@ -1,77 +1,28 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import { Alert, Button, Card, Flex, Progress, Divider, Form } from 'antd';
 import { Skill } from "@/remark/render-skill";
 
-import {
-  Host,
-  compileWithHost,
-  resolveBuildConfig,
-} from "@devicescript/compiler";
+
 import { DeviceScriptManagerCmd, OutPipe } from 'jacdac-ts';
 
 import { useJacdacStore } from "../../store/jacdacStore";
-import extrea_services from './services.json'
 
-// Device script implementation for kitten extension
-export class DevsHost implements Host {
-  private files: {};
-  private cfg: any;
-  bytecode: Uint8Array;
 
-  constructor(opt: any = {}) {
-    this.files = opt.files || {};
-    this.cfg = resolveBuildConfig();
-    this.cfg.hwInfo = opt.hwInfo || {
-      progName: "hello",
-      progVersion: "0.0.1",
-    };
-    this.cfg.addServices = extrea_services
-    this.cfg.services = [...this.cfg.services, ...extrea_services]
-  }
-
-  write(filename: string, contents: string | Uint8Array): void {
-    console.log("write", filename)
-    this.files[filename] = contents;
-  }
-  read(filename: string): string {
-    console.log("read", filename)
-    if (this.files.hasOwnProperty(filename)) {
-      return this.files[filename] as string;
-    }
-    throw new Error("No such file: " + filename);
-  }
-  resolvePath(p: string) {
-    console.log("resolve", p)
-    return p;
-  }
-
-  log(msg: string): void {
-    console.log(msg);
-  }
-
-  verifyBytecode(buf: Uint8Array): void {
-    // TODO: vm to verify bytecode??
-    this.bytecode = buf;
-    return;
-  }
-
-  getConfig() {
-    return this.cfg;
-  }
-}
 
 const DevsDownloadCard = ({config}) => {
 
   const {connected} = useJacdacStore()
 
   const skill: Skill = useMemo(() => {
-    return JSON.parse(config);
+    if (config)
+      return JSON.parse(config);
   }, [config]);
 
   return (
     <Card hoverable style={{ width: '50vw', margin: 10 }} bodyStyle={{padding: 0, overflow: 'hidden'}}>
       { connected ? <ConnectedState skill={skill}/>
-       : <DisconnectState />
+      : <DisconnectState />
       }
     </Card>
   );
@@ -83,9 +34,26 @@ const ConnectedState = ({skill}) => {
   const [ downloadErr, setDownloadErr ] = useState('')
   const [ downloadProgress, setDownloadProgress ] = useState(0)
 
+  const [compileWithHost, setCompileWithHost] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('@devicescript/compiler').then(module => {
+        setCompileWithHost(() => module.compileWithHost);
+      });
+    }
+  }, []);
+
+
   const handleDownload = async () => {
+    if (!compileWithHost) {
+      console.error("Compiler not loaded");
+      return;
+    }
     
     console.log("download", skill);
+    const { DevsHost } = await import('./DevsHost'); // 路径需要根据你的项目结构调整
+
     const host = new DevsHost({
       hwInfo: {
         // progName: "DeviceScript-workspace devs/hello",
