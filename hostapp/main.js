0e27b17d253f85891fd8183377192522d19be07c
riven <4771215@qq.com>
2023-12-13 11:07:15 +0800

    init tray menu

--- hostapp/main.js
+++ hostapp/main.js
@@ -1,16 +1,44 @@
-const {app, ipcMain, BrowserWindow} = require('electron')
+const {app, ipcMain, BrowserWindow, Tray} = require('electron')
 const path = require('path')
 
+let tray = null
+let mainwin = null
+
 app.on('ready', () => {
-    const win = new BrowserWindow({
+    mainwin = new BrowserWindow({
+        width: 320,
+        height: 240,
+        show: false,
+        // framed: false,
+        fullscreenable: false,
+        resizable: false,
+        useContentSize: true,
+        // transparent: true,
+        alwaysOnTop: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
+            backgroundThrottling: false,
             preload: path.join(__dirname, 'preload.js')
         }
     })
 
-    win.loadFile('index.html')
+    mainwin.setMenu(null)
+
+    mainwin.loadFile('index.html')
+
+    tray = new Tray('./assets/icon-16x16.png')
+
+    tray.on('click', () => {
+        mainwin.isVisible() ? mainwin.hide() : mainwin.show()
+    })
+
+    mainwin.on('close', (event) => {
+        event.preventDefault()
+        mainwin.hide()
+    })
+
+    // mainwin.hide()
 })
 
 app.on('window-all-closed', () => {
