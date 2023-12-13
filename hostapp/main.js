bd30319b6309b5c18dfa98822e0507b130850250
riven <4771215@qq.com>
2023-12-13 12:55:43 +0800

    add jdbus

--- hostapp/main.js
+++ hostapp/main.js
@@ -11,11 +11,25 @@ const path = require("path");
 const WebSocket = require("faye-websocket")
 const isDev = require("electron-is-dev");
 
+// for jacdac transport
+const {
+  // events
+  DEVICE_CHANGE,
+  CONNECTION_STATE,
+  FRAME_PROCESS,
+  FRAME_PROCESS_LARGE,
+  // transport
+  JDBus,
+  createNodeWebSerialTransport
+} = require('jacdac-ts')
+const { SerialPort } = require('serialport')
+
 const JACDAC_PORT = 8081;
 
 let tray = null;
 let mainwin = null;
 let server = null;
+let jdbus = null;
 
 function hideApp() {
   mainwin.hide();
@@ -25,6 +39,36 @@ function hideApp() {
 }
 
 const jdproxy = '<html lang="en"><title>Jacdac DevTools</title><meta name="viewport"content="width=device-width,initial-scale=1"><link rel="icon"href="https://microsoft.github.io/jacdac-docs/favicon.svg"type="image/x-icon"><style>iframe{position:absolute;left:0;top:0;height:100%;width:100%;border:none}</style><iframe id="frame"alt="Jacdac Dashboard"allow="usb;serial;bluetooth;vr;clipboard-write;"allowfullscreen sandbox="allow-scripts allow-downloads allow-same-origin"></iframe><script>!function(){function e(){var n;o||(n=!1,(o=new WebSocket(a)).binaryType="arraybuffer",console.debug("devtools: connecting ".concat(a,"...")),o.addEventListener("open",function(){console.debug("devtools: connected ".concat(o.url)),n=!0,d.contentWindow.postMessage({type:"devtoolsconnected",sender:s},"*")}),o.addEventListener("message",function(e){"string"==typeof e.data?d.contentWindow.postMessage(e.data,"*"):(e=new Uint8Array(e.data),d.contentWindow.postMessage({type:"messagepacket",channel:"jacdac",data:e,sender:s},"*"))}),o.addEventListener("close",function(){console.debug("devtools: connection closed"),o=void 0}),o.addEventListener("error",function(e){n&&console.debug("devtools: error",e),null!=o&&o.close()}))}var o,n=window.location,t="https:"===n.protocol,n=n.hostname,c=t?443:8081,a="".concat(t?"wss:":"ws:","//").concat(n,":").concat(c,"/"),d=document.getElementById("frame"),t=window.location.search||"",s=(t+=(0<t.length?"&":"?")+"devtools="+encodeURIComponent(a),Math.random()+""),n="https://microsoft.github.io/jacdac-docs/dashboard/"+t+"#"+s;window.addEventListener("message",function(e){e=e.data;e&&("messagepacket"===e.type&&"jacdac"===e.channel||"devicescript"===e.channel)&&(null==o?void 0:o.readyState)===WebSocket.OPEN&&o.send(e.data)});setInterval(e,5e3),e(),d.src=n}()</script>';
+
+async function startJacdacBus() {
+  const transports = [
+    await createNodeWebSerialTransport(SerialPort),
+  ]
+  const bus = new JDBus(transports, {
+    client: false,
+    disableRoleManager: true,
+    proxy: true
+  })
+
+  bus.passive = false;
+  bus.on('error', error => {
+    console.error("JDBUS", error);
+  })
+
+  bus.on(DEVICE_CHANGE, async () => {
+    const devices = bus.devices();
+    console.log("update devices", devices.length)
+    for (const device of devices) {
+      console.log("device", device.shortId)
+    }
+  })
+
+  bus.on(CONNECTION_STATE, async (state) => {
+    console.log("connection state", state)
+  })
+  
+  jdbus = bus;
+}
 
 function startHttpServer(){
   if (server) {
@@ -38,7 +82,14 @@ function startHttpServer(){
     });
 
     http_server.on('error', (err) => {
-      console.error("Http server error:", err);
+      if (/EADDRINUSE/.test(err.message)){
+        console.log("Port in use, closing server");
+        setTimeout(() => {
+          http_server.close();
+        }, 1000); 
+      } else {
+        console.error(err);
+      }
     })
 
     http_server.on('upgrade', (req, socket, body) => {
@@ -46,15 +97,36 @@ function startHttpServer(){
         const remoteAddress = socket.remoteAddress
 
         const client = new WebSocket(req, socket, body)
-        console.log("Websocket connection", req.url, remoteAddress)
+        const client_id = Math.random().toString(36).substring(2, 15)
+        console.log("Websocket connection", req.url, remoteAddress, client_id)
 
+        client.on('message', (event) => {
+          const { data } = event;
+          if (typeof data === 'string') {
+            console.log("Websocket message", data)
+          } else {
+            // forward to jacdac
+            const buffer = new Uint8Array(data);
+            data._jacdac_sender = client_id;
+            jdbus.sendFrameAsync(buffer);
+          }
+        })
 
-
+        client.on('close', () => {
+          console.log("Websocket close", req.url, remoteAddress, client_id)
+        })
       }
     })
 
     http_server.listen(JACDAC_PORT, '127.0.0.1')
     server = http_server;
+    // wait for server to start
+    setTimeout(() => {
+      if (server.listening) {
+        console.log("Server listening on port", JACDAC_PORT);
+        startJacdacBus();
+      }
+    }, 1000);
   } catch (e) {
     console.error(e);
   }
