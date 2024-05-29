9045f94eefc725f5dd280837da87da64d366ae69
riven <4771215@qq.com>
2024-05-29 11:27:32 +0800

    more skills

--- src/components/Builder/skill.tsx
+++ src/components/Builder/skill.tsx
@@ -2,7 +2,6 @@
 export class SkillBuilder {
   libs: string[] = ['import * as ds from "@devicescript/core"'];
   instances: Record<string, string> = {};
-  main: string = '';
   skills: Skill[] = []
 
   addLib(lib: string) {
@@ -17,10 +16,6 @@ export class SkillBuilder {
     this.instances[id] = instance
   }
 
-  addMain(main: string) {
-    this.main += main + '\n'
-  }
-
   buildAll() {
     for (const skill of this.skills) {
       skill.build(this)
@@ -32,34 +27,48 @@ export class SkillBuilder {
     for (const [id, instance] of Object.entries(this.instances)) {
       code += `const ${id} = ${instance}\n`
     }
-    code += this.main
+    for (const skill of this.skills) {
+      if (skill.code_generated && skill.parent === undefined) {
+        code += skill.code_generated
+      }
+    }
     return code
   }
 
 }
 
 
-abstract class Skill {
-  id: string;
-  name: string;
+export abstract class Skill {
+  static builder: SkillBuilder
   description: string;
   thumbnail: string;
   devs: string; // device script to run
   instance?: string;
   libs: string[] = [] // libraries to import in the devicescript builder
-  params?: Record<string, any> // parameters to replace in the script
-  isEvent?: boolean // is an event
+  params: Record<string, any> = {}// parameters to replace in the script
+  code_generated?: string // generated code
+  parent?: Skill // parent skill
 
-  constructor(id: string, name: string, options: any = {}) {
-    this.id = id
-    this.name = name
-    this.params = options.params || {}
+  constructor(public id: string, public name: string, public key0: string, public options: any = {}) {
+    if (!Skill.builder) {
+      Skill.builder = new SkillBuilder()
+    }
+    if (this.key0) { // the default parameter key, most skill has only one parameter
+      this.key0 = key0
+      if (typeof options === 'string') {
+        this.params = { [this.key0]: options }
+        this.options = {}
+      } else if (options instanceof Skill) {
+        this.params = { [this.key0]: options }
+        this.options = {}
+      }
+    } else {
+      this.params = { ...this.params, ...options.params }
+    }
+    Skill.builder.skills.push(this)
   }
 
   build(builder: SkillBuilder) {
-    if (this.isEvent){
-      return
-    }
     for (const lib of this.libs) {
       builder.addLib(lib)
     }
@@ -68,22 +77,23 @@ abstract class Skill {
     }
     let code = this.devs
     for (const [key, value] of Object.entries(this.params)) {
-      const re = new RegExp(key, 'g')
+      const re = new RegExp(`\\${key}`, 'g')
       if (typeof value === 'string') {
         code = code.replace(re, value)
       } else if (value instanceof Skill) {
         const _event = value as Skill
+        _event.parent = this
         code = code.replace(re, _event.getCode(this.params))
       }
     }
-    builder.addMain(code)
+    this.code_generated = code
   }
 
   getCode(params: Record<string, any> = {}) {
     const _params = { ...this.params, ...params}
     let code = this.devs
     for (const [key, value] of Object.entries(_params)) {
-      const re = new RegExp(key, 'g')
+      const re = new RegExp(`\\${key}`, 'g')
       code = code.replace(re, value)
     }
     return code
@@ -92,10 +102,10 @@ abstract class Skill {
 
 export class SkillButton extends Skill {
   event: string;
-  constructor(name: string, event: string, options: any = {}) {
-    super(`button_${name}`, name, options)
+  constructor(name: string, options: any = {},  event: string = 'down') {
+    super(`button_${name}`, name, '$EVENT', options)
     this.instance = `new ds.Button()`
-    this.devs = `btn.${event}.subscribe(async()=>{\n  $EVENT \n})`
+    this.devs = `${this.id}.${event}.subscribe(async()=>{\n  $EVENT \n})`
     if (!this.params['$EVENT']) {
       this.params['$EVENT'] = 'console.log("button clicked")'
     }
@@ -104,8 +114,34 @@ export class SkillButton extends Skill {
 
 export class SkillOpenUrl extends Skill {
   constructor(name: string, options: any = {}) {
-    super(`cd`, name, options)
+    super(`cd`, name, '$URL', options)
     this.instance = `await new ds.PCEvent()`
     this.devs = `await cd.openUrl("$URL")`
+  }
+}
+
+export class SkillStartApp extends Skill {
+  constructor(name: string, options: any = {}) {
+    super(`cd`, name, '$APP', options)
+    this.instance = `await new ds.PCEvent()`
+    this.devs = `await cd.openApp("$APP")`
+  }
+}
+
+export class SkillAiService extends Skill {
+  constructor(name: string, options: any = {}) {
+    super(`cd`, name, '$SCRIPT', options)
+    this.instance = `await new ds.PCEvent()`
+    // run ai script with name $SCRIPT, $PARAMS 
+    this.devs = `await cd.runScript("aiscript://$SCRIPT?$PARAMS")`
+
+  }
+}
+
+export class SkillSendText extends Skill {
+  constructor(name: string, options: any = {}) {
+    super(`cd`, name, '$TXT', options)
+    this.instance = `await new ds.PCEvent()`
+    this.devs = `await cd.sendText("$TXT")`
   }
 }
--- src/test/skillbuild.test.ts
+++ src/test/skillbuild.test.ts
@@ -1,9 +1,8 @@
-import { SkillBuilder, SkillButton } from '../components/Builder/skill'
+import { Skill, SkillButton, SkillOpenUrl } from '../components/Builder/skill'
 
 test('SkillButton', () => {
-  const _builder = new SkillBuilder()
-  const _btn = new SkillButton('id', 'name', {params: {}})
-  _btn.build(_builder)
-  const code = _builder.buildAll()
+  const _openurl = new SkillOpenUrl('openurl', 'https://example.com')
+  const _btn = new SkillButton('a', _openurl)
+  const code = Skill.builder.buildAll()
   console.log(code)
 })
